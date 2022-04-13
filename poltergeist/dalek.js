15b3096ed4acbfb81fe2e73634d08d1dc10dd183
da99 <1551848+da99@users.noreply.github.com>
2022-04-13 11:20:06 -0500

    Development checkpoint.

--- poltergeist/dalek.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-'Page title is correct': function (test) {
-  test
-    .open('https://localhost:4568/')
-    // .open('http://www.mises.org/')
-    .assert.title().is('Almost there...', 'It has title')
-    .done();
-}
-};
