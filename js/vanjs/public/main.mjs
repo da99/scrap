81510d9621e543fa75d815d3d9d230a9ad2e93be
da99 <1551848+da99@users.noreply.github.com>
2025-10-15 22:35:39 -0500

    First steps into VanJS.

---
+++ js/vanjs/public/main.mjs
@@ -0,0 +1,16 @@
+
+import van from "./van-1.6.0.min.js"
+const {a, div, li, p, ul, span, strong} = van.tags
+const total = van.state(1);
+const the_link = a("Hello");
+the_link.setAttribute("href", "/a\">ddf</a>")
+
+const Hello = () => div(
+  p("👋Hello"),
+  ul(
+    li("🗺️World"),
+  ),
+  span(strong("This is a span."), "and this is normal.")
+)
+
+van.add(document.body, Hello())
